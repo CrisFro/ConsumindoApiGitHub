2a39a83ce031e3767cf3ed59151490fc33342188
Cristiane Guimarães Fröhlich <cristiane_g_frohlich@hotmail.com>
2022-12-19 15:23:48 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+
+namespace ConsumindoApiGitHub
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Informe o Usuário: ");
+            string user = Console.ReadLine();
+
+
+            HttpClient client = new HttpClient { BaseAddress = new Uri($"https://api.github.com") };
+            client.DefaultRequestHeaders.Add("User-Agent", "MyConsoleApp");
+
+
+            var response = await client.GetAsync($"users/{user}/repos");
+
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var users = JsonConvert.DeserializeObject<User[]>(content);
+
+            foreach (var item in users)
+            {
+                Console.WriteLine("Nome: " + item.Name);
+                Console.WriteLine("Descrição: " + item.Description);
+                Console.WriteLine();asasas
+
+            }
+
+        }
+
+    }
+
+}
+
---
+++ User.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsumindoApiGitHub
+{
+    public class User //mapeando a classe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
